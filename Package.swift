2a080857bfd4a7cb2b49b549b39adeb92c28bde0
Sven A. Schmidt <sas@finestructure.co>
2020-03-16 07:43:18 +0100

    Compiling version

--- Package.swift
+++ Package.swift
@@ -1,28 +1,22 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "HistoryView",
+    platforms: [.macOS(.v10_15), .iOS(.v13)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "HistoryView",
             targets: ["HistoryView"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/finestructure/CompArch", from: "0.7.1"),
+        .package(url: "https://github.com/insidegui/MultipeerKit", from: "0.1.2"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HistoryView",
-            dependencies: []),
-        .testTarget(
-            name: "HistoryViewTests",
-            dependencies: ["HistoryView"]),
+            dependencies: ["CompArch", "MultipeerKit"]),
     ]
 )
